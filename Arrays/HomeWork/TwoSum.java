127064816dc81ef919e579196c8bcfdde7359c51
bala74923 <bala74923@gmail.com>
2022-09-27 23:39:56 +0530

    27-09-2022

---
+++ Arrays/HomeWork/TwoSum.java
@@ -0,0 +1,17 @@
+package Arrays.HomeWork;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int need= target-nums[i];
+            if(map.get(need)!=null)
+                return new int[]{map.get(need),i};
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
