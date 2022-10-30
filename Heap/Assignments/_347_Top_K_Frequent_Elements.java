f8b906bec83408b07cf861d8ecfb9f1771aec560
bala74923 <bala74923@gmail.com>
2022-10-31 00:31:06 +0530

    heap finished

---
+++ Heap/Assignments/_347_Top_K_Frequent_Elements.java
@@ -0,0 +1,17 @@
+package Heap.Assignments;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class _347_Top_K_Frequent_Elements {
+    public int[] solve(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int n: nums) map.put(n,map.getOrDefault(n,0)+1);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
+        for(int n:map.keySet()) pq.add(n);
+        int[] res = new int[k];
+        for(int i=0;i<k;i++) res[i] = pq.poll();
+        return res;
+    }
+}
